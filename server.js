c1495e061f11f9b67cb8aa3e0f797b9a10e946f4
MrPandit12345 <117350192+MrPandit12345@users.noreply.github.com>
2025-09-09 23:51:44 +0530

    fixed

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/v1/users',require('./routes/userRoute'))
 app.use('/api/v1/transcations',require('./routes/transactionRoute'))
 
 // Port
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 // Listen server
 app.listen(PORT,()=>{
